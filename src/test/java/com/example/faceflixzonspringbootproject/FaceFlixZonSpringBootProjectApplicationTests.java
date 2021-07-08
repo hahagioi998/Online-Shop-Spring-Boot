ab32af5ffecf858d22971a8409e099e524805c4b
shamiul38 <hasan.shamiul@bkash.com>
2021-07-08 14:36:07 +0600

    Create the initial project from springintializr

---
+++ src/test/java/com/example/faceflixzonspringbootproject/FaceFlixZonSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faceflixzonspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceFlixZonSpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
